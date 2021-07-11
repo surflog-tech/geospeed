d61ea107c0f3e9e1744e513553fedfb92f3052ea
Jacob Friis Saxberg <jacob@webcom.dk>
2021-07-11 15:09:20 +0200

    cleanup

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,4 @@
 import { readFileSync } from 'fs';
-import simplify from './simplify';
 import handler from './index';
 
 // https://connect.garmin.com/modern/activity/6829812928
@@ -8,10 +7,6 @@ const geoFile = './assets/test.json';
 const geoBuffer: ArrayBuffer = readFileSync(geoFile);
 
 describe('GeoSpeed', () => {
-  xit('should simplify GeoJSON', () => {
-    const geoJSON = JSON.parse(geoBuffer.toString());
-    console.log(JSON.stringify(simplify(geoJSON)));
-  });
   it('should measure speed', () => {
     const result = handler(geoBuffer);
     console.log(result);
