6a90b37c99904a31c1449562c6b30e5137bd2119
Jacob Friis Saxberg <jacob@webcom.dk>
2021-07-11 16:30:32 +0200

    todo

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,12 @@
 import { FeatureCollection, Feature, LineString } from 'geojson';
 
 export type SurflogResult = {
-  topspeed: number;
+  topspeed: number; // fastest speed
+  topspeed250?: number; // fastest speed over 250 meter
+  topspeed500?: number; // fastest speed over 500 meter
+  legs?: number; // number of rides forth and back
+  jibespeed?: number; // fastest turn
+  planing?: number; // Time planing in % (>18km/h)
 }
 
 type SurflogGeoJsonProperties = {
