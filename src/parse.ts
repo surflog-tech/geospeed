5b8549da264b194a42853482a36a16bb5769c10c
Jacob Friis Saxberg <jacob@webcom.dk>
2022-01-01 16:10:21 +0100

    TS fix

--- src/parse.ts
+++ src/parse.ts
@@ -1,8 +1,7 @@
 import { SurflogFeature } from './index.d';
 
 function parseGeoBuffer(geoBuffer: ArrayBuffer): SurflogFeature {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return JSON.parse(geoBuffer.toString());
+  return JSON.parse(geoBuffer.toString()) as SurflogFeature;
 }
 
 export default parseGeoBuffer;
