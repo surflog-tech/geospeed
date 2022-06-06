4279ee8289f1f162d4f1bc56c2998de0789b2380
Jacob Friis Saxberg <jacob@webcom.dk>
2022-06-06 22:52:18 +0200

    test data

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -10,7 +10,10 @@ function readFile(filePath: string) {
 describe('GeoSpeed', () => {
 
   it('should measure speed', () => {
-    const geoFile = './assets/test4.json';
+    // const geoFile = './assets/test4.json';
+    // const geoFile = './assets/test5.json';
+    // const geoFile = './assets/0320149c-dc7c-55bf-a7fa-c21f84faa20d.json';
+    const geoFile = './assets/41e2d09d-2d6d-52c7-a39a-c03b4d1e0961.json';
     const result = handler(readFile(geoFile));
     console.log(result);
   });
