ea96d3988833f56853ef900444f71ccce84c4151
Jacob Friis Saxberg <jacob@webcom.dk>
2022-06-09 20:38:08 +0200

    more test data

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -14,6 +14,7 @@ describe('GeoSpeed', () => {
     // const geoFile = './assets/test5.json';
     // const geoFile = './assets/0320149c-dc7c-55bf-a7fa-c21f84faa20d.json';
     const geoFile = './assets/41e2d09d-2d6d-52c7-a39a-c03b4d1e0961.json';
+    // const geoFile = './assets/05fff327-0a86-5c89-85d7-ac70c5a8abb5.json';
     const result = handler(readFile(geoFile));
     console.log(result);
   });
